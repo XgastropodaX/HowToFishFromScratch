62535905030f901facd4e9a0e4b1ba00698f8c08
nova <80142002+XgastropodaX@users.noreply.github.com>
2026-09-02 15:01:02 -0400

    progress of ui part 2

---
+++ xtreme-pro-bass-fishing-2009/spatial.gd
@@ -0,0 +1,32 @@
+extends Node3D
+
+var pressed := false
+var rightPressed := false
+var scaleMod := 0.005
+var moveSpeed := 0.1
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("mouse_up"):
+		$obj_1_gesila2.scale += Vector3.ONE*scaleMod
+	if event.is_action_pressed("mouse_down"):
+		$obj_1_gesila2.scale -= Vector3.ONE*scaleMod
+	
+	if rightPressed and event is InputEventMouseMotion:
+		global_transform.origin += Vector3(event.relative.x, -event.relative.y, 0)*moveSpeed
+	
+	if pressed and event is InputEventMouseMotion:
+		rotation.x += event.relative.y*0.005
+		$obj_1_gesila2.rotation.y += event.relative.x*0.005
+
+
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("right_click"):
+		rightPressed = true
+	if Input.is_action_just_released("right_click"):
+		rightPressed = false
+
+	if Input.is_action_just_pressed("click"):
+		pressed = true
+	if Input.is_action_just_released("click"):
+		pressed = false
