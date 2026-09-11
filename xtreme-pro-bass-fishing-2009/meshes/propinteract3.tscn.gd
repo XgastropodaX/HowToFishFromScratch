0fbb4a300c59d8e866a84e606248ddb0ffb05df2
nova <80142002+XgastropodaX@users.noreply.github.com>
2026-09-11 16:16:29 -0400

    new highlight system

--- xtreme-pro-bass-fishing-2009/meshes/propinteract3.tscn.gd
+++ xtreme-pro-bass-fishing-2009/meshes/propinteract3.tscn.gd
@@ -1,17 +1,24 @@
 extends Area3D
-@onready var model: MeshInstance3D = $".."
+@onready var model: MeshInstance3D = $"../Box Spring"
 @onready var distance = 6
 @onready var click_distance = get_meta("Click_Distance")
 @onready var highlight = false
-@onready var player: CharacterBody3D = $"../../../../GameManager/Player"
-@onready var game_manager: Node = $"../../../../GameManager"
-@onready var menus: CanvasLayer = $"../../../../Menus"
-
+@onready var menus: CanvasLayer = $"../../../GameManager/Menus"
+@onready var head: Node3D = $".."
+@onready var player: CharacterBody3D = $"../../../GameManager/Player"
+@onready var game_manager: Node = $"../../../GameManager"
+@export var highlight_material: Material
 
 var highlight_mat: StandardMaterial3D = \
 	preload('res://materials/2new_standard_material_3d.tres')
 var materials: Array[StandardMaterial3D]
-	
+var og_materials: Dictionary = {}
+
+#Gets the original Materials of the objects being highlighted.
+func _store_materials(node: Node):
+	for child in node.get_children():
+		if child is MeshInstance3D:
+			og_materials[child] = child.material_override
 	
 func _ready() -> void:
 	self.call_deferred("connection")
@@ -19,28 +26,35 @@ func _ready() -> void:
 func connection():
 	for i in model.get_surface_override_material_count():
 		materials.append(model.get_surface_override_material(i))
-	mouse_entered.connect(toggle_highlight.bind(true))
-	mouse_exited.connect(toggle_highlight.bind(false))
+	mouse_entered.connect(toggle_highlight.bind(head, true))
+	mouse_exited.connect(toggle_highlight.bind(head, false))
 	player.movement.connect(check_highlight)
 	
 	
-func toggle_highlight(on: bool):
+func toggle_highlight(node: Node, on: bool):
 	if on:
 		highlight = true
-		distance = abs((model.global_position.x - $"../../../../GameManager/Player".global_position.x)) + abs((model.global_position.z - $"../../../../GameManager/Player".global_position.z))
+		distance = abs((model.global_position.x - $"../../../GameManager/Player".global_position.x)) + abs((model.global_position.z - $"../../../GameManager/Player".global_position.z))
 		print(distance)
 		if distance < click_distance:
-			for i in materials.size():
-				model.set_surface_override_material(i, highlight_mat)
+			for child in node.get_children():
+				if child is MeshInstance3D:
+					if on:
+						child.material_override = highlight_mat
+					else: 
+						child.material_override = og_materials.get(child)
+					
 	else:
 		highlight = false
-		for i in materials.size():
-				model.set_surface_override_material(i, materials[i])
+		for child in node.get_children():
+			if child is MeshInstance3D:
+				child.material_override = og_materials.get(child)
+
 
 func check_highlight():
 	if highlight:
 		print("highlight")
-		distance = abs((model.global_position.x - $"../../../../GameManager/Player".global_position.x)) + abs((model.global_position.z - $"../../../../GameManager/Player".global_position.z))
+		distance = abs((model.global_position.x - $"../../../GameManager/Player".global_position.x)) + abs((model.global_position.z - $"../../../GameManager/Player".global_position.z))
 		print(distance)
 		if distance < click_distance:
 			for i in materials.size():
@@ -59,6 +73,7 @@ func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, n
 
 
 func _on_mouse_entered() -> void:
+	_store_materials(head)
 	menus.item_label.text = get_meta("Name")
 
 	
